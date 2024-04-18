d9ed8198c4222fb4eb94d1ad3e94e6f404a66c09
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-04-18 13:47:28 +0700

    Create axeron.sh

---
+++ shell/axeron.sh
@@ -0,0 +1,1 @@
+cp /sdcard/Android/data/com.fhrz.axeron/files/axeron.function /data/local/tmp; chmod +x /data/local/tmp/axeron.function; source /data/local/tmp/axeron.function; check_axeron; !myCommands
