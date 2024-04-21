22decc259b576d84569c74a6bb0eb2492a3b4004
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-04-21 17:47:56 +0700

    Update axeron.sh

--- shell/axeron.sh
+++ shell/axeron.sh
@@ -1,18 +1,18 @@
 cp /sdcard/Android/data/com.fhrz.axeron/files/axeron.function /data/local/tmp; chmod +x /data/local/tmp/axeron.function; source /data/local/tmp/axeron.function; check_axeron; echo "[Execution-start]"; !myCommands
 local myCommands="!myCommands"
-local myOperator=$(echo $myCommands | awk '{print $1}' )
-local myArgument=$(echo $myCommands | cut -d " " -f 2-)
-IFS=$'\n' read -r myOperator myArgument <<< "$myCommands"
+IFS=$'\n'
+for cmd in $myCommands; do
+    myOperator=$(echo "$cmd" | awk '{print $1}')
+    myArgument=$(echo "$cmd" | cut -d ' ' -f 2-)
 
-# Memeriksa apakah myOperator tersedia
-if ! type "$myOperator" > "${EXECPATH}/axeron_log.txt" 2>&1; then
-    # Memeriksa apakah direktori /sdcard/AxeronModules/myOperator ada
-    if [ -d "/sdcard/AxeronModules/${myOperator}" ]; then
-        # Menjalankan perintah myOperator dengan myArgument
-        "/sdcard/AxeronModules/${myOperator}" "$myArgument"
-    else
-        echo "sh: axeron-function[${myOperator}?]: inaccessible functions"
+    # Memeriksa apakah myOperator tersedia
+    if ! type "$myOperator" > "${EXECPATH}/axeron_log.txt" 2>&1; then
+        # Menjalankan perintah myOperator jika direktori sesuai ditemukan
+        if [ -d "/sdcard/AxeronModules/${myOperator}" ]; then
+            "/sdcard/AxeronModules/${myOperator}" "$myArgument"
+        else
+            echo "sh: axeron-function[${myOperator}?]: inaccessible functions"
+        fi
     fi
-fi
-
+done
 echo "[Execution-end]\n"
