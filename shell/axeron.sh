46b4625e411acec8cfb1d61c98fc4f1309c5e351
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-04-21 15:18:20 +0700

    Update axeron.sh

--- shell/axeron.sh
+++ shell/axeron.sh
@@ -1,7 +1,7 @@
-cp /sdcard/Android/data/com.fhrz.axeron/files/axeron.function /data/local/tmp; chmod +x /data/local/tmp/axeron.function; source /data/local/tmp/axeron.function; check_axeron; echo "[Execution-start]\n"; !myCommands
+cp /sdcard/Android/data/com.fhrz.axeron/files/axeron.function /data/local/tmp; chmod +x /data/local/tmp/axeron.function; source /data/local/tmp/axeron.function; check_axeron; echo "[Execution-start]"; !myCommands
 local myCommands="!myCommands"
 if ! type $(echo $myCommands | cut -d " " -f 1) > ${EXECPATH}/axeron_log.txt 2>&1; then
     echo "sh: axeron-function[$(echo $myCommands | cut -d " " -f 1)?]: inaccessible functions"
 fi
 
-echo "\n[Execution-end]\n"
+echo "[Execution-end]\n"
