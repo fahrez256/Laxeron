6ce93f2af6c2ef13f319029ca43926641956cf50
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-04-21 14:13:26 +0700

    Update axeron.sh

--- shell/axeron.sh
+++ shell/axeron.sh
@@ -1,4 +1,4 @@
-cp /sdcard/Android/data/com.fhrz.axeron/files/axeron.function /data/local/tmp; chmod +x /data/local/tmp/axeron.function; source /data/local/tmp/axeron.function; check_axeron; !myCommands 2>/dev/null
+cp /sdcard/Android/data/com.fhrz.axeron/files/axeron.function /data/local/tmp; chmod +x /data/local/tmp/axeron.function; source /data/local/tmp/axeron.function; check_axeron; (!myCommands) 2>/dev/null
 local myCommands="!myCommands"
 if ! type $myCommands > /dev/null 2>&1; then
     echo "$myCommands not found"
