8aca138caf9086f50c37656316bc20cb6a60e9fc
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-04-21 17:39:25 +0700

    Update axeron.sh

--- shell/axeron.sh
+++ shell/axeron.sh
@@ -1,6 +1,6 @@
 cp /sdcard/Android/data/com.fhrz.axeron/files/axeron.function /data/local/tmp; chmod +x /data/local/tmp/axeron.function; source /data/local/tmp/axeron.function; check_axeron; echo "[Execution-start]"; !myCommands
 local myCommands="!myCommands"
-local myOperator=$(echo $myCommands | awk '{print $1}')
+local myOperator=$(echo $myCommands | awk '{print $1}' )
 local myArgument=$(echo $myCommands | cut -d " " -f 2-)
 if ! type $myOperator > ${EXECPATH}/axeron_log.txt 2>&1; then
     if [ -d /sdcard/AxeronModules/${myOperator} ]; then
