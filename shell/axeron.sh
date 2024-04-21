f1719bd0189c226477229fe0f876131311d576d0
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-04-21 18:33:01 +0700

    Update axeron.sh

--- shell/axeron.sh
+++ shell/axeron.sh
@@ -1,6 +1,5 @@
-cp /sdcard/Android/data/com.fhrz.axeron/files/axeron.function /data/local/tmp; chmod +x /data/local/tmp/axeron.function; source /data/local/tmp/axeron.function; check_axeron; echo "[Execution-start]"; !myCommands
+cp /sdcard/Android/data/com.fhrz.axeron/files/axeron.function /data/local/tmp; chmod +x /data/local/tmp/axeron.function; source /data/local/tmp/axeron.function; check_axeron; echo "[Execution-start]"; !myCommands 2>${EXECPATH}/error.txt
 local myCommands="!myCommands"
-(eval "!myCommands" | cut -d ':' -f 2) 2>${EXECPATH}/error.txt
 IFS=$'\n;'
 for cmd in $myCommands; do
     # Menghapus spasi tambahan dari setiap perintah
