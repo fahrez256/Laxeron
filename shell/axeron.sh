0136ff67e7d96c3760ab172c1fd7106401d720a6
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-04-21 14:18:16 +0700

    Update axeron.sh

--- shell/axeron.sh
+++ shell/axeron.sh
@@ -1,5 +1,5 @@
 cp /sdcard/Android/data/com.fhrz.axeron/files/axeron.function /data/local/tmp; chmod +x /data/local/tmp/axeron.function; source /data/local/tmp/axeron.function; check_axeron; (!myCommands) 2>/dev/null
 local myCommands="!myCommands"
 if ! type $myCommands > /dev/null 2>&1; then
-    echo "$myCommands not found"
+    echo "[$myCommands] [ ? ] is not detected in depedencies"
 fi
