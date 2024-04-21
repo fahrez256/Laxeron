ffd0e76f8829e7f4013200441d1e84101c4689ab
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-04-21 14:58:09 +0700

    Update axeron.sh

--- shell/axeron.sh
+++ shell/axeron.sh
@@ -1,4 +1,4 @@
-cp /sdcard/Android/data/com.fhrz.axeron/files/axeron.function /data/local/tmp; chmod +x /data/local/tmp/axeron.function; source /data/local/tmp/axeron.function; check_axeron; echo "[Start Execution]"; echo ""; eval "!myCommands"
+cp /sdcard/Android/data/com.fhrz.axeron/files/axeron.function /data/local/tmp; chmod +x /data/local/tmp/axeron.function; source /data/local/tmp/axeron.function; check_axeron; echo "[Start Execution]"; echo ""; eval "!myCommands" 2>${EXECPATH}/error.txt
 # local myCommands="!myCommands"
 # if ! type $(echo $myCommands | cut -d " " -f 1) > ${EXECPATH}/axeron_log.txt 2>&1; then
 #     echo "[$(echo $myCommands | cut -d " " -f 1)] [ ? ] is not detected in depedencies"
