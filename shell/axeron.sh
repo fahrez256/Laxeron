0d5332398fe94924e58b35b0a174886f0c5e54a7
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-04-21 17:43:32 +0700

    Update axeron.sh

--- shell/axeron.sh
+++ shell/axeron.sh
@@ -2,9 +2,14 @@ cp /sdcard/Android/data/com.fhrz.axeron/files/axeron.function /data/local/tmp; c
 local myCommands="!myCommands"
 local myOperator=$(echo $myCommands | awk '{print $1}' )
 local myArgument=$(echo $myCommands | cut -d " " -f 2-)
-if ! type $myOperator > ${EXECPATH}/axeron_log.txt 2>&1; then
-    if [ -d /sdcard/AxeronModules/${myOperator} ]; then
-        ash $myOperator $myArgument
+IFS=$'\n' read -r myOperator myArgument <<< "$myCommands"
+
+# Memeriksa apakah myOperator tersedia
+if ! type "$myOperator" > "${EXECPATH}/axeron_log.txt" 2>&1; then
+    # Memeriksa apakah direktori /sdcard/AxeronModules/myOperator ada
+    if [ -d "/sdcard/AxeronModules/${myOperator}" ]; then
+        # Menjalankan perintah myOperator dengan myArgument
+        "/sdcard/AxeronModules/${myOperator}" "$myArgument"
     else
         echo "sh: axeron-function[${myOperator}?]: inaccessible functions"
     fi
