2b746873bc404935f6975ca260256cbfda5b3f23
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-04-21 15:15:18 +0700

    Update axeron.sh

--- shell/axeron.sh
+++ shell/axeron.sh
@@ -1,7 +1,7 @@
 cp /sdcard/Android/data/com.fhrz.axeron/files/axeron.function /data/local/tmp; chmod +x /data/local/tmp/axeron.function; source /data/local/tmp/axeron.function; check_axeron; echo "[Execution-start]\n"; !myCommands
 local myCommands="!myCommands"
 if ! type $(echo $myCommands | cut -d " " -f 1) > ${EXECPATH}/axeron_log.txt 2>&1; then
-    echo "[$(echo $myCommands | cut -d " " -f 1)] [ ? ] is not detected in depedencies"
+    echo "ash: axeron-function[$(echo $myCommands | cut -d " " -f 1)?]: is not detected in depedencies"
 fi
 
 echo "\n[Execution-end]\n"
