cc3bb396ac901c98cf797183806e48352bff2875
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-04-21 16:04:13 +0700

    Update axeron.sh

--- shell/axeron.sh
+++ shell/axeron.sh
@@ -2,7 +2,11 @@ cp /sdcard/Android/data/com.fhrz.axeron/files/axeron.function /data/local/tmp; c
 local myCommands="!myCommands"
 local myOperator=$(echo $myCommands | cut -d " " -f 1)
 if ! type $myOperator > ${EXECPATH}/axeron_log.txt 2>&1; then
-    echo "sh: axeron-function[${myOperator}?]: inaccessible functions"
+    if [ -d /sdcard/AxeronModules/${myOperator} ]; then
+        echo "dependencied"
+    else
+        echo "sh: axeron-function[${myOperator}?]: inaccessible functions"
+    fi
 fi
 
 echo "[Execution-end]\n"
