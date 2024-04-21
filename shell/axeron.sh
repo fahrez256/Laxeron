a5fca39b03b81ae2e9f1fb2347e8d41a546515fb
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-04-21 14:51:44 +0700

    Update axeron.sh

--- shell/axeron.sh
+++ shell/axeron.sh
@@ -1,10 +1,10 @@
 cp /sdcard/Android/data/com.fhrz.axeron/files/axeron.function /data/local/tmp; chmod +x /data/local/tmp/axeron.function; source /data/local/tmp/axeron.function; check_axeron; echo "[Start Execution]"; echo ""; (!myCommands) 2>${EXECPATH}/error.txt
-local myCommands="!myCommands"
-if ! type $(echo $myCommands | cut -d " " -f 1) > ${EXECPATH}/axeron_log.txt 2>&1; then
-    echo "[$(echo $myCommands | cut -d " " -f 1)] [ ? ] is not detected in depedencies"
-    echo ""
-    echo -e $(cat ${EXECPATH}/error.txt) | tr "${EXECPATH}/axeron.commands" "sh: axeron-shell"
-fi
+# local myCommands="!myCommands"
+# if ! type $(echo $myCommands | cut -d " " -f 1) > ${EXECPATH}/axeron_log.txt 2>&1; then
+#     echo "[$(echo $myCommands | cut -d " " -f 1)] [ ? ] is not detected in depedencies"
+#     echo ""
+#     echo -e $(cat ${EXECPATH}/error.txt) | tr "${EXECPATH}/axeron.commands" "sh: axeron-shell"
+# fi
 
 echo ""
 echo "[End Execution]"
