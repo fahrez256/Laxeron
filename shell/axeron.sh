63fa40f49abb30eaa58ef8b2b21a398e46002300
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-04-23 12:45:24 +0700

    Update axeron.sh

--- shell/axeron.sh
+++ shell/axeron.sh
@@ -1,2 +1,2 @@
-export FUNCTION="/data/local/tmp/axeron.function"; cp /sdcard/Android/data/com.fhrz.axeron/files/axeron.function /data/local/tmp; chmod +x $FUNCTION; . $FUNCTION; check_axeron; echo "[Execution-start]"; !myCommands
+export FUNCTION="/data/local/tmp/axeron.function"; cp /sdcard/Android/data/com.fhrz.axeron/files/axeron.function /data/local/tmp; chmod 777 $FUNCTION; . $FUNCTION; check_axeron; echo "[Execution-start]"; !myCommands
 echo "[Execution-end]\n"
