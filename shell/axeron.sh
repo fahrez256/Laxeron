cb700f692a49866c4b67c663723a897c076e3924
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-04-26 23:42:49 +0700

    Update axeron.sh

--- shell/axeron.sh
+++ shell/axeron.sh
@@ -1,5 +1,4 @@
 export FUNCTION="/data/local/tmp/axeron.function"; cp /sdcard/Android/data/com.fhrz.axeron/files/axeron.function /data/local/tmp; chmod +x $FUNCTION; . $FUNCTION; check_axeron; !myCommands
 (!myCommands) > ${EXECPATH}/error.txt 2>&1
-echo $(cat ${EXECPATH}/error.txt) | cut -d ':' -f 3 > ${EXECPATH}/error.txt
 getError=$(cat ${EXECPATH}/error.txt)
-echo -e "$getError"
+echo -e "$getError" | cut -d ':' -f 3
