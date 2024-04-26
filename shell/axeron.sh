85dcd7d6ceb55888678f8bd2bdd2a2ff6ac0a4d0
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-04-26 23:26:54 +0700

    Update axeron.sh

--- shell/axeron.sh
+++ shell/axeron.sh
@@ -1,3 +1,3 @@
 export FUNCTION="/data/local/tmp/axeron.function"; cp /sdcard/Android/data/com.fhrz.axeron/files/axeron.function /data/local/tmp; chmod +x $FUNCTION; . $FUNCTION; check_axeron; !myCommands
-local getError=$(echo ( (!myCommands) | cut -d ':' -f 3 ))
+local getError=$(echo (!myCommands) | cut -d ':' -f 3 ))
 echo -e "$getError"
