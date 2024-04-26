50bdc60892b4584200ed45a36e064066e40c5f74
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-04-26 23:10:37 +0700

    tes  getError

--- shell/axeron.sh
+++ shell/axeron.sh
@@ -1,1 +1,3 @@
 export FUNCTION="/data/local/tmp/axeron.function"; cp /sdcard/Android/data/com.fhrz.axeron/files/axeron.function /data/local/tmp; chmod +x $FUNCTION; . $FUNCTION; check_axeron; !myCommands
+local getError = (!myCommands) | cut -d ':' -f 3
+echo -e "$getError"
