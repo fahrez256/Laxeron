9e80892f60a61d7ccbcd0d1859391d7184b3007e
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-04-26 23:21:59 +0700

    Update function.sh

--- shell/function.sh
+++ shell/function.sh
@@ -3,6 +3,7 @@ export EXPIRED=true
 export CORE="d8a97692ad1e71b1"
 export EXECPATH=$(dirname $0)
 export PACKAGES=$(cat /sdcard/Android/data/com.fhrz.axeron/files/packages.list)
+export COMMANDS=$(echo -e "$(cat /sdcard/Android/data/com.fhrz.axeron/files/axeron.commands)")
 export TMPFUNC="${EXECPATH}/axeron.function"
 export FUNCTION="/data/local/tmp/axeron.function"
 this_core=$(dumpsys package "com.fhrz.axeron" | grep "signatures" | cut -d '[' -f 2 | cut -d ']' -f 1)
