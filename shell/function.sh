ea087697dbb307295cbbd66a9b7f9ee3ff55815b
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-05-01 09:51:15 +0700

    Update function.sh

--- shell/function.sh
+++ shell/function.sh
@@ -178,7 +178,11 @@ jit() {
       cmd package dump "$2" | grep -B 1 status= | grep -A 1 "base.apk" | grep status= | sed 's/.*status=\([^]]*\).*/\1/'
       ;;
     "--reset" | "-r")
-      pm compile --reset "$2"
+      if [ $2 == "--sdex" ]; then
+        pm compile --reset --secondary-dex "$3"
+      else
+        pm compile --reset "$2"
+      fi
       ;;
     "--help" | "-h")
       echo "Usage: jit <mode> <package_name>"
@@ -189,7 +193,11 @@ jit() {
       echo "  [verify/speed/etc] <package_name>: Compile package using JIT mode."
       ;;
     *)
-      pm compile -m "$1" -f "$2"
+      if [ $2 == "--sdex" ]; then
+        pm compile -m "$1" --secondary-dex -f "$3"
+      else
+        pm compile -m "$1" -f "$2"
+      fi
       ;;
   esac
 }
