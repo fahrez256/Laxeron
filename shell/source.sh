96105dd5e88c2f810cb8ca2dbe926db7e63d08e8
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-05-26 18:26:13 +0700

    Update source.sh

--- shell/source.sh
+++ shell/source.sh
@@ -2,4 +2,4 @@ thisfunc="/data/local/tmp/axeron.function"
 cp /sdcard/Android/data/com.fhrz.axeron/files/axeron.function /data/local/tmp
 chmod +x $thisfunc
 . $thisfunc
-check_axeron
+check_axeron $THISPKG
