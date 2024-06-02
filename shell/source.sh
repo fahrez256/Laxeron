409ab8c37f4f0947a9306f9da6ad40823521c558
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-06-02 21:14:31 +0700

    Update source.sh

--- shell/source.sh
+++ shell/source.sh
@@ -2,4 +2,11 @@ thisfunc="/data/local/tmp/axeron.function"
 cp /sdcard/Android/data/com.fhrz.axeron/files/axeron.function /data/local/tmp
 chmod +x $thisfunc
 . $thisfunc
-check_axeron $AXERONPKG
+this_core=$(dumpsys package "$AXERONPKG" | grep "signatures" | cut -d '[' -f 2 | cut -d ']' -f 1)
+check_axeron() {
+  [[ -z $AXERONPKG || $AXERONPKG != "com.fhrz.axeron" ]] && echo "Hacked by Aldo (Chermods) (Maintenance)" && exit 0
+  if ! echo "$CORE" | grep -q "$this_core"; then
+    echo "Axeron Not Original"
+    exit 0
+  fi
+}
