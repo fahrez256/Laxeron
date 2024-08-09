d4fae6b0ff6fcf8ffff2510a0bdf2657472c4610
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-08-09 23:40:20 +0700

    Update function.sh

--- shell/function.sh
+++ shell/function.sh
@@ -96,7 +96,7 @@ flaunch() {
     return 0
   fi
   
-  am start --activity-no-animation -n $(cmd package dump "$1" | awk '/MAIN/{getline; print $2}' | head -n 1)
+  am start --activity-no-animation -n $(cmd package dump "$1" | awk '/MAIN/{getline; print $2}' | head -n 1) > /dev/null
    # am startservice -n com.fhrz.axeron/.Services.FastLaunch --es pkg "$1" > /dev/null
 }
 
