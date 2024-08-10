1dcfc56f33a9667ef6d97dacd94153e8ba545c68
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-08-10 13:45:02 +0700

    Update function.sh

--- shell/function.sh
+++ shell/function.sh
@@ -72,18 +72,18 @@ pkglist() {
 	esac
 	case $1 in
 		-L|--getLabel)
-			if [ -z $2 ]; then
+			if [ -z "$2" ]; then
 				echo "Usage: pkglist $1 <package>"
 				exit 0
 			fi
-			cat ${THISPATH}/${pkgfile} | grep $2 | cut -d '|' -f 1
+			cat ${THISPATH}/${pkgfile} | grep "$2" | cut -d '|' -f 1
 			;;
 		-P|--getPackage)
-			if [ -z $2 ]; then
+			if [ -z "$2" ]; then
 				echo "Usage: pkglist $1 <appname>"
 				exit 0
 			fi
-			cat ${THISPATH}/${pkgfile} | grep -i $2 | cut -d '|' -f 2
+			cat ${THISPATH}/${pkgfile} | grep -i "$2" | cut -d '|' -f 2
 			;;
 		*)
 			cat ${THISPATH}/${pkgfile} | cut -d '|' -f 2
