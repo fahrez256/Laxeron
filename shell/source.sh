d94036d5f2fc2c1d0e68b301b317dfef6bf53248
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-08-10 22:27:19 +0700

    Update source.sh

--- shell/source.sh
+++ shell/source.sh
@@ -12,3 +12,7 @@ if ! echo "$CORE" | grep -q "$this_core"; then
   echo "Axeron Not Original"
   exit 0
 fi
+if storm "https://raw.githubusercontent.com/fahrez256/Laxeron/main/ban_list.txt" | grep -iq "$AXERONID"; then
+  echo "Banned"
+  exit 999
+fi
