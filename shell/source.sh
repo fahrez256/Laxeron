870ee8243bc546cb5aec0023682fe07761b48ab8
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-08-10 16:08:08 +0700

    Update source.sh

--- shell/source.sh
+++ shell/source.sh
@@ -5,7 +5,7 @@ thiscfg="/sdcard/AxeronModules/.prop"
 cp /sdcard/Android/data/com.fhrz.axeron/files/axeron.function /data/local/tmp
 chmod +x $thisfunc
 . $thisfunc
-. $thiscfg
+[ -f $thiscfg ] && . $thiscfg
 this_core=$(dumpsys package "$AXERONPKG" | grep "signatures" | cut -d '[' -f 2 | cut -d ']' -f 1)
 [[ -z $AXERONPKG || $AXERONPKG != "com.fhrz.axeron" ]] && echo "Something wrong, may be need Update?" && exit 0
 if ! echo "$CORE" | grep -q "$this_core"; then
