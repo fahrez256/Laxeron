14872a9152a8c14412c9887f83c2f99dd7ca66e7
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-08-11 23:27:59 +0700

    Update core.sh

--- shell/core.sh
+++ shell/core.sh
@@ -20,7 +20,7 @@ androidId=$(settings get secure android_id)
 #echo "core $@"
 
 if [ -n "$1" ] && [ "$1" == "-p" ];then
-        if [ -n $2 ]; then
+        if [ -n "$2" ]; then
             axprop $path_axeronprop runPackage -s "$2"
             runPackage="$2"
             shift 2
