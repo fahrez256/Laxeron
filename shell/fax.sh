89919a740dda776a2fb46d21a7e9ed4ed6cbf376
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-08-12 15:35:56 +0700

    Add files via upload

--- shell/fax.sh
+++ shell/fax.sh
@@ -7,7 +7,7 @@ logview() {
 	echo -e "${ORANGE}${1}${NC} ${GREY}${2}${NC}"; 
 }
 
-logview "[Parameter #$#]:" "$@"
+#logview "[Parameter #$#]:" "$@"
 
 fax() {
 	local ORANGE='\033[38;2;255;85;3m'
@@ -41,8 +41,11 @@ fax() {
 
 	tmpVCode=${versionCode:-0}
 	tmpTStamp=${timeStamp:-0}
-	log "[Init Version Code]" "$tmpVCode"
-	log "[Init Last Update]" "$(timeformat $tmpTStamp)"
+	
+	if [ -n "$pathCash" ]; then
+		log "[Init Version Code]" "$tmpVCode"
+		log "[Init Last Update]" "$(timeformat $tmpTStamp)"
+	fi
 
 	ctr=0
 	idFound=false
@@ -56,13 +59,14 @@ fax() {
 		cachePathProc="${cachePath}/proc${ctr}"
 		cachePathProp="${cachePathProc}/${pathProp}"
 		
-		log "\n[Zip]" "$file"
-		log "[File Last Update]" "$(timeformat $timeStamp)"
 		unzip -o "$file" -d "$cachePathProc" > /dev/null
 		dos2unix "$cachePathProp"
 		source "$cachePathProp"
 		
-		[ -n "$id" ] && echo "$id" | grep -iq "$nameDir" && log "[Module ID found]" "$id" || continue
+		[ -n "$id" ] && echo "$id" | grep -iq "$nameDir" || continue
+		idFound=true
+		log "\n[Zip]" "$file"
+		log "[File Last Update]" "$(timeformat $timeStamp)"
 		
 		if [ "$versionCode" -ge "$tmpVCode" ]; then
 			tmpVCode=$versionCode
@@ -87,7 +91,7 @@ fax() {
 		
 		log "[pathParent]" "$cachePathParent"
 		
-		rm -r "$pathCash" > /dev/null 2>$1 && log "[Old module has been removed.]"
+		rm -rf "$pathCash" && log "[Old module has been removed.]"
 		mkdir -p "$pathCash" && log "[Installing new module.]"
 		
 		for item in "${cachePathParent%/}"/*; do
@@ -100,6 +104,45 @@ fax() {
 		axprop --log "$showLog" "$pathCashProp" timeStamp "$tmpTStamp"
 		log "[\$] [Module successfully updated.]" "$(timeformat $tmpTStamp)"
 	done
+	
+	if [ "$idFound" = false ]; then
+		log "[AX processing complete. No matching ID found.]"
+		echo "ID not found"
+		exit 1
+	fi
+	
+	dos2unix "$pathCashProp"
+	source "$pathCashProp"
+	log "[Final prop from]" "$pathCashProp"
+	find "$pathCash" -type f -exec chmod +x {} \;
+	log "[Set executable permissions on files.]"
+
+	install=$(find "$pathCash" -type f -iname "${install:-"install"}*")
+	remove=$(find "$pathCash" -type f -iname "${remove:-"remove"}*")
+	log "[Install script]" "$install"
+	log "[Remove script]" "$remove"
+	execution_time=$(($(date +%s%3N) - start_time))
+	log "[AX processing complete.]" "$execution_time milliseconds\n"
+	
+	case $2 in
+		-r|--remove)
+			if [ -n "$remove" ]; then
+				shift 2
+				"${remove}" $@
+				rm -rf "$pathCash"
+			else
+				echo "[ ! ] Cannot remove this module: Remove script not found."
+			fi
+			;;
+		*)
+			if [ -n "$install" ]; then
+				shift
+				"${install}" $@
+			else
+				echo "[ ! ] Cannot install this module: Install script not found."
+			fi
+			;;
+	esac
 }
 
 fax $@
