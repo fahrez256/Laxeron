1a0c4b38e47e5599889a53bdcbf76fba2d1fe17c
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-08-14 11:33:39 +0700

    Update function.sh

--- shell/function.sh
+++ shell/function.sh
@@ -18,6 +18,7 @@ local NC='\033[0m'
 local modulePath="/sdcard/AxeronModules"
 local cachePath="/sdcard/AxeronModules/.cache"
 local cash="/data/local/tmp/axeron_cash"
+local urlBin="https://raw.githubusercontent.com/fahrez256/Laxeron/main/shell/bin"
 timeformat() { echo "$(date -d "@$1" +"%Y-%m-%d %H.%M.%S")"; }
 
 #constructor
@@ -120,7 +121,7 @@ import() {
 }
 
 toast() {
-	storm -rP "$AXBIN" -x "https://raw.githubusercontent.com/fahrez256/Laxeron/main/shell/bin/toast.sh" -fn "toast" "$@"
+	storm -rP "$AXBIN" -x "${urlBin}/toast.sh" -fn "toast" "$@"
 }
 
 openlink() {
@@ -132,7 +133,7 @@ buyvip() {
 }
 
 pkglist() {
-	storm -rP "$AXBIN" -x "https://raw.githubusercontent.com/fahrez256/Laxeron/main/shell/bin/pkglist.sh" -fn "pkglist" "$@"
+	storm -rP "$AXBIN" -x "${urlBin}/pkglist.sh" -fn "pkglist" "$@"
 }
 
 flaunch() {
@@ -206,7 +207,7 @@ whitelist() {
 }
 
 jit() {
-	storm -rP "$AXBIN" -x "https://raw.githubusercontent.com/fahrez256/Laxeron/main/shell/bin/jit.sh" -fn "jit" "$@"
+	storm -rP "$AXBIN" -x "${urlBin}/jit.sh" -fn "jit" "$@"
 }
 
 optimize() {
@@ -222,86 +223,19 @@ optimize() {
 }
 
 axprop() {
-	if [ "$#" -lt 2 ]; then
-		echo "Usage: axprop <filename> <key> [-s|--String] <value> | axprop <filename> -d <key> | axprop <filename> -g <key>"
-		return 1
-	fi
-
-	local ORANGE='\033[38;2;255;85;3m'
-    	local GREY='\033[38;2;105;105;105m' # Kode warna ANSI untuk oranye
-    	local NC='\033[0m'
- 	local showLog=false
-
-    	log() { 
-        	[ "$showLog" = true ] && echo -e "${ORANGE}${1}${NC} ${GREY}${2}${NC}"; 
-    	}
-
-    	if [ "$1" == "--log" ]; then
-     		case $2 in
-        		"true"|"false") showLog=$2; shift 2 ;;
-	  		*) shift ;;
-	 	esac
-    	fi
-
-	local filename=$1 key=$2 value
-	local sanitized_key=$(echo "$key" | tr -cd '[:alnum:]_-')
-
-	[ ! -f "$filename" ] && echo "File $filename not found!" && return 1
-
-	case $key in
-		-d|--delete)
-			key=$3
-			sanitized_key=$(echo "$key" | tr -cd '[:alnum:]_-')
-			if grep -q "^$sanitized_key=" "$filename"; then
-				sed -i "/^$sanitized_key=/d" "$filename"
-				log "[Deleted key]" "$key"
-			else
-				log "[Key $key not found]"
-			fi
-			;;
-		-g|--get)
-			key=$3
-			sanitized_key=$(echo "$key" | tr -cd '[:alnum:]_-')
-			if grep -q "^$sanitized_key=" "$filename"; then
-				grep "^$sanitized_key=" "$filename" | cut -d '=' -f2-
-			else
-				log "[Key $key not found]"
-			fi
-			;;
-		*)
-			if [ "$3" = "-s" ] || [ "$3" = "--String" ]; then
-				value="\"$4\""
-			else
-				value=$3
-			fi
-			if grep -q "^$sanitized_key=" "$filename"; then
-				sed -i "s/^$sanitized_key=.*/$sanitized_key=$value/" "$filename"
-			else
-				echo "$sanitized_key=$value" >> "$filename"
-			fi
-			log "[\$] [Updated $(basename $filename) with $sanitized_key]" "$value"
-			;;
-	esac
-}
-
-cashclear() {
-	if rm -rf "/data/local/tmp/axeron_cash"; then
- 		echo "CASH: Success to clear"
-   	else
-    		echo "CASH: Failed to clear"
-    	fi
+	storm -rP "$AXBIN" -x "${urlBin}/axprop.sh" -fn "axprop" "$@"
 }
 
 shx() {
-	storm -rP "$AXBIN" -x "https://raw.githubusercontent.com/fahrez256/Laxeron/main/shell/bin/shx.sh" -fn "shx" "$@"
+	storm -rP "$AXBIN" -x "${urlBin}/shx.sh" -fn "shx" "$@"
 }
 
 ax() {
-	storm -rP "$AXBIN" -x "https://raw.githubusercontent.com/fahrez256/Laxeron/main/shell/bin/ax.sh" -fn "ax" "$@"
+	storm -rP "$AXBIN" -x "${urlBin}/ax.sh" -fn "ax" "$@"
 }
 
 ax2() {
-	storm -rP "$AXBIN" -x "https://raw.githubusercontent.com/fahrez256/Laxeron/main/shell/bin/ax2.sh" -fn "ax2" "$@"
+	storm -rP "$AXBIN" -x "${urlBin}/ax2.sh" -fn "ax2" "$@"
 }
 
 axfun_construct $@
