d91aa9b13f23f7fd76dd07d6e4bfa06215cd216a
Fahrez256Bit <167403685+fahrez256@users.noreply.github.com>
2024-08-14 11:04:51 +0700

    Delete shell/axeron.sh

--- shell/axeron.sh
+++
@@ -1,1 +0,0 @@
-export FUNCTION="/data/local/tmp/axeron.function"; cp /sdcard/Android/data/com.fhrz.axeron/files/axeron.function /data/local/tmp; chmod +x $FUNCTION; . $FUNCTION; check_axeron; !myCommands
